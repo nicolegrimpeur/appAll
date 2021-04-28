cfb92f85eda083c11daa5a312264d8edde147da3
Nicolas Barrat <nicolas.barrat@student.junia.com>
2021-04-28 15:01:10 +0200

    Correction bug infos + bug icone sur fond blanc

--- src/app/shared/models/textes-results.model.ts
+++ src/app/shared/models/textes-results.model.ts
@@ -4,5 +4,5 @@ import {IdAffichageModel} from './id-affichage.model';
 export class TextesResultsModel {
   liens: Array<TextesModel>;
   infos: Array<TextesModel>;
-  idAffichage: Array<IdAffichageModel>;
+  idAffichage: IdAffichageModel;
 }
