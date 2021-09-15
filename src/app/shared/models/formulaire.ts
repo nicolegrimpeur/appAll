c8442567849fb0cea54f9c965ce60bfb97d4a636
Nicolas Barrat <nicolas.barrat@student.junia.com>
2021-09-15 19:16:39 +0200

    Modification affichage informations

--- src/app/shared/models/formulaire.ts
+++ src/app/shared/models/formulaire.ts
@@ -15,23 +15,24 @@ export function CleanForm(tabIdDiv: string[]): void {
 // permet de créer la div parent
 export function DebutForm(tabIdDiv: string[]): void {
   let currentDiv;
-  let div;
+  let ionCard;
 
   // ajoute à la div ayant le nom contenu après le _ la div idDIv
   for (const idDiv of tabIdDiv) {
     currentDiv = document.getElementById(idDiv.substring(idDiv.search('_') + 1));
-    div = document.createElement('div');
-    if (currentDiv !== null) { currentDiv.appendChild(div); }
-    div.setAttribute('id', idDiv);
+    ionCard = document.createElement('ion-card');
+    if (currentDiv !== null) { currentDiv.appendChild(ionCard); }
+    ionCard.setAttribute('id', idDiv);
   }
 }
 
 // permet d'ajouter du texte au formulaire
 export function AjoutTexte(valueString, idString, idDiv): void {
-  const div = document.getElementById(idDiv);
+  const ionCard = document.getElementById(idDiv);
 
-  const p = document.createElement('p');
-  div.appendChild(p);
-  p.setAttribute('class', idString);
-  p.innerHTML = '&nbsp; ' + valueString;
+  const ionCardContent = document.createElement('ion-card-content');
+  ionCard.appendChild(ionCardContent);
+  ionCardContent.setAttribute('class', idString);
+  ionCardContent.setAttribute('style', 'padding: 5px; font-size: 17px;');
+  ionCardContent.innerHTML = '&nbsp; ' + valueString;
 }
