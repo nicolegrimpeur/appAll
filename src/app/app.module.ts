ede8255562f8e3dc30683c4d38945cb4de6ac480
Nicolas <nicolas.barrat@student.junia.com>
2022-08-07 15:47:57 +0200

    Update matomo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,16 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
 import {IonicStorageModule} from '@ionic/storage-angular';
-import { NgxMatomoTrackerModule } from '@ngx-matomo/tracker';
-import { NgxMatomoRouterModule } from '@ngx-matomo/router';
+import {NgxMatomoTrackerModule} from '@ngx-matomo/tracker';
+import {NgxMatomoRouterModule} from '@ngx-matomo/router';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), NgxMatomoTrackerModule.forRoot({ trackerUrl: 'https://nicob.ovh:7085/', siteId: '2' }), NgxMatomoRouterModule],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), NgxMatomoTrackerModule.forRoot({
+    trackerUrl: 'https://nicob.ovh/matomo/',
+    siteId: '2'
+  }), NgxMatomoRouterModule],
   providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
   bootstrap: [AppComponent],
 })
